d1475e50a586172670cecdbb302277f8c253659d
Julius Ceasar <joks026@gmail.com>
2021-05-24 15:57:15 +0800

    seeding stuff

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Str;
+
 
 class User extends Authenticatable
 {
@@ -42,14 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function question()
+    public function questions()
     {
         return $this->hasMany(Question::class);
-    }
-
-    public function setTitleAttribute($value)
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
     }
 }
